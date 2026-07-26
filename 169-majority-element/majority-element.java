e1be6726ba67e38b80c028e40df60fba85da1c96
SAURABH PRAKASH <prakash.saurabh4success@gmail.com>
2026-07-26 22:58:39 +0530

    Time: 1 ms (99.89%) | Memory: 55.5 MB (66.59%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int votes = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (votes == 0) {
+                majority = nums[i];
+                votes = 1;
+            } else if (nums[i] == majority) {
+                votes++;
+            } else {
+                votes--;
+            }
+        }
+
+        return majority;
+    }
+}
