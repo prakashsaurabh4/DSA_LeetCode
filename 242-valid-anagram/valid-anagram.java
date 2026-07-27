6410b49ad5dd540f1c5eb8c1726a08b38fd30efa
SAURABH PRAKASH <prakash.saurabh4success@gmail.com>
2026-07-27 12:32:58 +0530

    Time: 7 ms (31.96%) | Memory: 44.5 MB (66.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+    //For Upper and Lower Mixed Letter
+    s=s.toLowerCase();
+    t=t.toLowerCase();
+
+    //Remove All space
+    s=s.replace(" ","");
+    t=t.replace(" ","");
+    
      int[] counts = new int[26];
      for(int i=0;i<s.length();i++){
         counts[s.charAt(i) - 'a']++;
