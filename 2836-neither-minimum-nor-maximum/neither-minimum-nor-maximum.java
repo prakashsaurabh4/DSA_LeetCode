73959b8608cd5d95d0e1af8435e197979003514c
SAURABH PRAKASH <prakash.saurabh4success@gmail.com>
2026-08-13 18:08:31 +0530

    Time: 14 ms (8.69%) | Memory: 47.1 MB (62.25%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -1,24 +1,22 @@
 //TC=0(nlogn)
-// class Solution {
-//     public int findNonMinOrMax(int[] nums) {
-//         int n=nums.length;
-//         Arrays.sort(nums);
-//         for(int i=0;i<n;i++){
-//             if(n<=2) return -1;
-//         } 
-//         return nums[1];
-//     }
-// }
-
-//TC=0(nlogn)
 class Solution {
     public int findNonMinOrMax(int[] nums) {
-        if(nums.length <= 2) return -1;
-        int a=nums[0];   
-        int b=nums[1];   
-        int c=nums[2];  
-        if((a>b && a<c) || (a<b && a>c)) return a;
-        else if((b>c && b<a) || (b<c && b>a)) return b;
-        else return c;
+        int n=nums.length;
+        if(n<=2) return -1;
+        Arrays.sort(nums);
+        return nums[1];
     }
 }
+
+//TC=0(1)
+// class Solution {
+//     public int findNonMinOrMax(int[] nums) {
+//         if(nums.length <= 2) return -1;
+//         int a=nums[0];   
+//         int b=nums[1];   
+//         int c=nums[2];  
+//         if((a>b && a<c) || (a<b && a>c)) return a;
+//         else if((b>c && b<a) || (b<c && b>a)) return b;
+//         else return c;
+//     }
+// }
