8a750c4d951c58a19aa433381181216a830a621c
SAURABH PRAKASH <prakash.saurabh4success@gmail.com>
2026-08-13 17:54:43 +0530

    Time: 4 ms (100.00%) | Memory: 47 MB (74.59%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -1,10 +1,24 @@
+//TC=0(nlogn)
+// class Solution {
+//     public int findNonMinOrMax(int[] nums) {
+//         int n=nums.length;
+//         Arrays.sort(nums);
+//         for(int i=0;i<n;i++){
+//             if(n<=2) return -1;
+//         } 
+//         return nums[1];
+//     }
+// }
+
+//TC=0(nlogn)
 class Solution {
     public int findNonMinOrMax(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(n<=2) return -1;
-        } 
-        return nums[1];
+        if(nums.length <= 2) return -1;
+        int a=nums[0];   
+        int b=nums[1];   
+        int c=nums[2];  
+        if((a>b && a<c) || (a<b && a>c)) return a;
+        else if((b>c && b<a) || (b<c && b>a)) return b;
+        else return c;
     }
 }
